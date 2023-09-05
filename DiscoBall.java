8e1ab13907659948fb03a5fb99e8c27137d8f899
Spencer Au <spencerau96@gmail.com>
2023-09-04 18:53:57 -0700

    ,

--- DiscoBall.java
+++ DiscoBall.java
@@ -1,6 +1,7 @@
 import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
 import org.apache.batik.util.XMLResourceDescriptor;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.svg.SVGDocument;
 import org.w3c.dom.svg.SVGRect;
@@ -8,13 +9,36 @@ import org.w3c.dom.svg.SVGRect;
 import java.io.File;
 import java.io.IOException;
 
+import java.lang.reflect.Array;
+import java.util.LinkedList;
 
 
 public class DiscoBall {
     private SVGDocument svgDocument;
 
+    // constructor for DiscoBall
     public DiscoBall(String svgFilePath) {
         this.svgDocument = loadSVGDocument(svgFilePath);
+        LinkedList[] rings = new LinkedList[21];
+        populateRings();
+    }
+
+    // implement populateRings() method
+    public populateRings() {
+        // for each ring in the disco ball
+        for (int i = 0; i < 21; i++) {
+            // create a new linked list
+            LinkedList<Node> ring = new LinkedList<Node>();
+            // add the first node to the linked list
+            ring.add(new Node("0"));
+            // for each square in the ring
+            for (int j = 1; j < 21; j++) {
+                // add a new node to the linked list
+                ring.add(new Node(Integer.toString(j)));
+            }
+            // add the linked list to the array
+            rings[i] = ring;
+        }
     }
 
     public void changeRingColor(int ringIndex, String colorHex) {
@@ -42,5 +66,37 @@ public class DiscoBall {
 
     private NodeList findRingElements() {
         // Implement logic to find and return the NodeList of ring elements
+    }
+}
+
+class Node {
+    private Node prev;
+    private Node next;
+    private String id;
+
+    public Node(String id) {
+        this.id = id;
+        this.prev = null;
+        this.next = null;
+    }
+
+    // Getters and setters for prev, next, and id
+    public Node getPrev() {
+        return this.prev;
+    }
+    public Node getNext() {
+        return this.next;
+    }
+    public String getId() {
+        return this.id;
+    }
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    public void setId(String id) {
+        this.id = id;
     }
 }
